3c52288ffd29184d1c13a10ae391b98cbefe0358
Mike <mike@otep.ch>
2015-10-07 08:50:38 +0200

    init repo

---
+++ SimpleCalculator.cpp
@@ -0,0 +1,48 @@
+#include "SimpleCalculator.h"
+
+#include <istream>
+#include <stdexcept>
+
+int calc(int a, int b, char c) {
+	switch (c) {
+	case '+':
+		return a + b;
+	case '-':
+		return a - b;
+	case '*':
+		return a * b;
+	case '/':
+		if (b == 0) {
+			throw std::invalid_argument { "division by zero" };
+		} else {
+			return a / b;
+		}
+	case '%':
+		if (b == 0) {
+			throw std::invalid_argument { "modulo by zero" };
+		} else {
+			return a % b;
+		}
+	default:
+		throw std::invalid_argument { "not a valid operator "+c };
+
+	}
+}
+
+int calc(std::istream& in) {
+	std::string input = "";
+	int number1 = 0;
+	int number2 = 0;
+	char myChar = { 0 };
+	in >> number1;
+
+	in >> myChar;
+
+	in >> number2;
+	try {
+		return calc(number1,number2,myChar);
+	}
+	catch( const std::invalid_argument& e ) {
+	    return 0;
+	}
+}
---
+++ SimpleCalculator.h
@@ -0,0 +1,10 @@
+
+#ifndef SIMPLECALCULATOR_H_
+#define SIMPLECALCULATOR_H_
+#include <iosfwd>
+int calc(int a, int b, char c) ;
+int calc(std::istream& in) ;
+
+
+
+#endif /* SIMPLECALCULATOR_H_ */
